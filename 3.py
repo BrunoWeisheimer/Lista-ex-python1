c79061a7c0d597d36f1b312deec504c0bdd28776
BrunoWeisheimer <164061815+BrunoWeisheimer@users.noreply.github.com>
2024-03-21 00:26:46 -0300

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,9 @@
+print(' Qual é o verdadeiro nome do super-herói Batman?')
+print('a)Bruce Wayne')
+print('b)Clark Kent')
+print('c)Peter Parker')
+print('d)Tony Stark')
+print('e)Steve Rogers')
+respostaUsuario=input('Digite sua resposta:')
+resposta = 'a'
+print('A sua resposta foi {}, a resposta correta é {}'.format(respostaUsuario, resposta))
