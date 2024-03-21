1f29d54707e7008fc3e65df370f4c15d0670fbbb
BrunoWeisheimer <164061815+BrunoWeisheimer@users.noreply.github.com>
2024-03-21 00:24:55 -0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,18 @@
+#python
+print("Olá Mundo!")
+
+#java
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+    }
+}
+
+#cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Olá Mundo!" << endl;
+    return 0;
+}
