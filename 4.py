cda112826669bcc129dbe7fb05bd1e318bb3a1b4
BrunoWeisheimer <164061815+BrunoWeisheimer@users.noreply.github.com>
2024-03-21 00:29:34 -0300

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,6 @@
+print('Qual a capital do Brasil?')
+print('Rio de janeiro , São Paulo , Brasilia , Belo Horizonte , Salvador')
+
+respostaUsuario = input('Digite sua resposta:')
+Resposta = 'Brasilia'
+print('A sua resposta foi {}, a resposta correta é {}!'.format(respostaUsuario, Resposta))
